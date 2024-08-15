5f7226814286986436af3df85e905a2b862bf5f3
juan-suarez <juan.suarez1@utp.edu.co>
2024-08-15 03:34:19 -0500

    postman collection added and now is running in 3001 port

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.use(cookieParser());
-    await app.listen(3000);
+    await app.listen(3001);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
